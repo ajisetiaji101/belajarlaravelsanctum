61e1a8fdd14b23818a900ff3e5403a24278e9256
ajisetiaji101 <ajisetiaji101@gmail.com>
2022-02-02 15:33:20 +0700

    Penambahan Auth dan Form Controller

---
+++ app/Http/Controllers/API/FormController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FormController extends Controller
+{
+    public function index(){
+        return response()->json([
+            "message" => "Wellcome to Form"
+        ],200);
+    }
+}
